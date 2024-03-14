6974fabd072f1384e0357194ae0bdbc6b6516776
gobikayogkeswaran <gobikayogkeswaran@gmail.com>
2024-03-14 12:05:49 +0530

    gdd6

--- db/conn.js
+++ db/conn.js
@@ -5,9 +5,8 @@ require("dotenv").config();
 
 
 const client = mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-  })
+  .connect('mongodb+srv://Gobika13:sudhagobi@cluster0.azzscvw.mongodb.net/doctor?retryWrites=true&w=majority') 
+  
   .then(() => {
     console.log("DB connected");
   })
